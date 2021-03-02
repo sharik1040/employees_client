abfc8d4c3b11e88f4d6648056e9fbfd91a58c9f1
sharik1040 <nastia.sharnenkova@gmail.com>
2021-03-03 00:16:24 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        Hello World
+      </header>
+    </div>
+  );
+}
+
+export default App;
